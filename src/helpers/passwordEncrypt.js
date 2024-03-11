fa87491ca3257b55de4094a4434e02e6558d5e84
nilpeep <niluferk038@gmail.com>
2024-03-12 00:01:09 +0300

    password encryption

---
+++ src/helpers/passwordEncrypt.js
@@ -0,0 +1,20 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG API
+------------------------------------------------------- */
+
+// password encryption
+
+const crypto = require('node:crypto')
+
+const keyCode = process.env?.SECRET_KEY || 'write_random_chars_in_here'
+const loopCount = 10_000 // 10K
+const charCount = 32 // write 32 for 64
+const encType = 'sha512'
+
+module.exports = function (password) {
+    return crypto.pbkdf2Sync(password, keyCode, loopCount, charCount, encType).toString('hex')
+    
+    
+}
+
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,27 +5,54 @@
 
 const mongoose = require('mongoose')
 
-// Schema :
+const passwordEncrypt = require('../helpers/passwordEncrypt')
 
+
+// Schema:
 const UserSchema = new mongoose.Schema({
-    email: { 
-        type: String, 
+
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        // unique: [true, 'Email must be unique.'],
+        // required: true,
+        required: [true, 'Email must be required.'],
+        // validate: (email) => { return true },
+        // validate: [
+        //     (email) => {
+        //         if (email.includes('@') && email.includes('.')) {
+        //             return true
+        //         }
+        //         return false
+        //     },
+        //     'Email type is incorrect'
+        // ],
+        validate: [
+            (email) => (email.includes('@') && email.includes('.')),
+            'Email type is incorrect'
+        ]
+
+    },
+
+    password: {
+        type: String,
+        trim: true,
         required: true,
-        trim:true,
-        unique:true
+        set: (password) => passwordEncrypt(password) 
     },
-    password: { 
-        type: String, 
-        trim:true,
-        required: true
-    },
-    firstName:String,
-    lastName:String
+
+    firstName: String,
+
+    lastName: String,
 
 }, {
-        // settings
-        collection:'user',
-        timestamps:true
+    collection: 'user',
+    timestamps: true
 })
+
+// module.exports = {
+//     User: mongoose.model('User', UserSchema)
+// }
 
 module.exports = mongoose.model('User', UserSchema)
