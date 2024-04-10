91441cb8b50d6c997374144fe932aef5fefa6c04
nilpeep <niluferk038@gmail.com>
2024-04-10 22:53:25 +0300

    user control

---
+++ src/middlewares/userControl.js
@@ -0,0 +1,28 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG API
+------------------------------------------------------- */
+
+const User = require('../models/user.model')
+
+module.exports = async (req, res, next) => {
+
+    if (req?.session?.id) {
+
+        const { id, password } = req.session
+
+        const user = await User.findOne({ _id: id })
+
+        if (user && user.password == password) {
+
+            req.user = user
+            req.isLogin = true
+
+        } else {
+
+            req.session = null
+            req.isLogin = false
+        }
+    }
+    next()
+}
